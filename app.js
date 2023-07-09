fc4355e9c5b0261cb7f33a6c52f336132901b5d5
Akim Minko <akim.minko@yandex.ru>
2023-07-09 19:16:18 +0300

    refactor: app.js

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const routes = require('./routes');
 
 const { PORT = 3000 } = process.env;
 
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  userNewUrlParser: true,
+  useCreateIndex: true,
+  userFindAndModify: false,
+});
+
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Application is running on port ${PORT}`);
