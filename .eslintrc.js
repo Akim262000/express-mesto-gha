a261398f15cf39ee86c5fd8e4c1a91fd033cfe1f
Akim Minko <akim.minko@yandex.ru>
2023-07-12 21:17:15 +0300

    refactor: package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,8 @@
 module.exports = {
     "env": {
-        // "browser": true,
-        "es2021": true
+        "es2021": true,
     },
     "extends": "airbnb-base",
-    // "plugin:react/recommended",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module"
