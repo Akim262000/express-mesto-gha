3d70b0c40bf948f632a7520b571056d43d79e7ad
Akim Minko <akim.minko@yandex.ru>
2023-07-23 17:26:49 +0300

    feat: create email and passward in model

--- models/user.js
+++ models/user.js
@@ -1,6 +1,21 @@
 const mongoose = require("mongoose");
+const isEmail = require("validator/lib/isEmail")
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: "Неправильный формат",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    // minlength: 8
+  },
   name: {
     type: String,
     minlength: 2,
